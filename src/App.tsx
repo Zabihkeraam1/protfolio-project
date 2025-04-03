cfe976e04358a28876d1297d597507b99446fca7
Zabih Keraam <keraamzabih@gmail.com>
2025-04-03 21:32:26 +0430

    Automated commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import React from 'react';
 
 function App() {
-  const [response, setResponse] = useState('')
-  const handleClick = (e:React.FormEvent)=>{
-    e.preventDefault()
-    axios.get('https://dpy9haeeaz.eu-west-3.awsapprunner.com/api').then(res=>{
-      setResponse(res.data.message)
-    })
-    console.log(response)
-  }
   return (
-    <>
-      <h1>React App</h1>
-      <div className="card">
-        <button onClick={handleClick}>
-          Send request
-        </button>
-        <p>
-          {response}
-        </p>
-      </div>
-    </>
-  )
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f4f4f4' }}>
+      <div style={{ padding: '20px', backgroundColor: 'white', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}>
+        <h1 style={{ fontSize: '24px', fontWeight: 'bold', textAlign: 'center', color: '#333' }}>Welcome to My React App</h1>
+  </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
